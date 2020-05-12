ec6625bb46d2bb05471094fb8396f86b3b8cbff0
Timothy John Morland <me@timothy-morland.com>
2020-05-12 12:23:53 -0400

    switch to using p5

--- src/client/App.js
+++ src/client/App.js
@@ -1,85 +1,60 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 
 import AceEditor from "react-ace";
-import "ace-builds/webpack-resolver";
-import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/webpack-resolver.js";
 
-import constructApi from "./constructApi";
+function Sketch({ code, style }) {
+  const p5 = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/p5.js";
+  const srcDoc =
+    "<html><head><script src=" +
+    p5 +
+    "></script><script>" +
+    code +
+    "</script></head><body></body></html>";
+
+  return <iframe style={style} srcDoc={srcDoc} frameBorder={0} />;
+}
+
+function Console({ log, style }) {
+  return (
+    <div style={style}>
+      <ul>
+        {log.forEach((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default function App() {
-  const [code, setCode] = useState("");
-  const [log, setLog] = useState("");
+  const template =
+    "function setup() {\n" +
+    "  // put setup code here\n" +
+    "  createCanvas(400, 400);\n" +
+    "}\n" +
+    "\n" +
+    "function draw() {\n" +
+    "  // put drawing code here\n" +
+    "  background(220);\n" +
+    "};\n";
 
-  const [api, setApi] = useState([]);
-  const [handlers, setHandlers] = useState({});
-
-  const [worker, setWorker] = useState(null);
-  const [canvas, setCanvas] = useState(null);
-  const [ctx, setCtx] = useState(null);
-
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-
-    const funcs = [
-      function setHeight(height) {
-        if (height) {
-          canvas.height = height;
-        }
-      },
-
-      function setWidth(width) {
-        if (width) {
-          canvas.width = width;
-        }
-      },
-    ];
-
-    const [newApi, newHandlers] = constructApi(funcs)
-    setApi(newApi);
-    setHandlers(newHandlers);
-  }, [canvasRef]);
-
-  const run = () => {
-    if (worker) worker.terminate();
-
-    const blob = new Blob([code, ...api], { type: "text/javascript" });
-    const workerUrl = URL.createObjectURL(blob);
-    setWorker(new Worker(workerUrl));
-  };
-
-  useEffect(() => {
-    if (!worker) return;
-
-    worker.addEventListener("message", ({ data }) => {
-      console.log(data);
-      console.log(handlers);
-      handlers[data.name](...data.params);
-    });
-
-    worker.addEventListener("error", (event) => {
-      console.log(event);
-    });
-  }, [worker]);
+  const [code, setCode] = useState(template);
 
   return (
-    <div style={{ display: "flex", alignItems: "flex-start", padding: 10 }}>
-      <AceEditor
-        value={code}
-        mode="javascript"
-        onChange={(code) => setCode(code)}
-        style={{ marginRight: 10 }}
-      />
-      <div>
-        <div style={{ marginBottom: 5 }}>
-          <button style={{ marginRight: 5 }} onClick={run}>
-            Run
-          </button>
-        </div>
-        <canvas ref={canvasRef} style={{ border: "1px solid black" }} />
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div style={{ flex: 0.75, display: "flex" }}>
+        <AceEditor
+          language="javascript"
+          value={code}
+          onChange={(code) => {
+            setCode(code);
+          }}
+          style={{ flex: 0.5, margin: 5 }}
+        />
+        <Sketch code={code} style={{ flex: 0.5, margin: 5 }} />
       </div>
+      <Console log={["test"]} style={{ flex: 0.25, margin: 5 }} />
     </div>
   );
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,4 @@
 const express = require("express");
 const app = express();
 
-const models = require("./models");
-
 app.use(express.static("dist"));
-
-models.sequelize.sync().then(() => {
-  app.listen(process.env.PORT || 8080, () => {
-    console.log(`Listening on port ${process.env.PORT || 8080}!`);
-  })
-});
