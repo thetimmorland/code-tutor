b6eaae53d3262bb7af5db732e1b4be6af5152774
Timothy John Morland <me@timothy-morland.com>
2020-05-14 12:50:35 -0400

    use cd in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-# deploy server to heroku
+project_root="${BASH_SOURCE%/*}"
+
+cd $project_root
+
+printf "Deploying client to github Pages\n"
+npm run deploy --prefix client
+
+printf "Deploying server to Heroku\n"
 git subtree push --prefix server heroku master
