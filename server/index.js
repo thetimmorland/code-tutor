a346f1671b1af0cd0b76bfcf6ba5de24aeda296c
Timothy John Morland <me@timothy-morland.com>
2020-05-14 12:35:25 -0400

    make server a hello world example

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
-app.use(express.static("dist"));
-
-app.listen(port, () => console.log(`Server listening on port ${port}`))
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Server listening on port ${port}`));
