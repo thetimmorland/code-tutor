d0ec6040367e594d8a143daaf11472b59aeba1e0
Timothy John Morland <me@timothy-morland.com>
2020-06-01 21:04:48 -0400

    fixup! configure eslint

--- src/client/App.js
+++ src/client/App.js
@@ -1,17 +1,21 @@
-import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Share from "./Share";
-import CreateSketch from "./CreateSketch";
-import Ide from "./Ide";
+import Share from './Share';
+import CreateSketch from './CreateSketch';
+import Ide from './Ide';
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" children={<CreateSketch />} />
+        <Route exact path="/">
+          <CreateSketch />
+        </Route>
         <Route path="/:id">
-          <Share children={<Ide />} />
+          <Share>
+            <Ide />
+          </Share>
         </Route>
       </Switch>
     </Router>
